59d499418c1a364b728a8611dcd95f2c1cfb8295
Krits Huang <47982451+Krstar233@users.noreply.github.com>
2020-11-22 16:09:33 +0800

    owo

---
+++ Cpp/leetcode/242.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> table;
+        if (s.length() != t.length())
+            return 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++){
+            table[s[i]]++;
+            table[t[i]]--;
+        }
+
+        for (map<char, int>::iterator it = table.begin(); it != table.end(); it++){
+            if (it->second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
